17fff3d0053dc8375a4ada3620ef0c06d589d5e9
Aleksi <aleksi.huotari@hotmail.com>
2024-11-02 12:02:49 +0200

    esim3 valmis

--- esim3/main.cpp
+++ esim3/main.cpp
@@ -7,22 +7,22 @@ int main() {
   int num;
   int result;
   int n, k;
-  //cout << "Lasketaan kertoma!" << endl;
-  //cout << "Anna positiivinen kokonaisluku" << endl;
-  //cin >> num;
-  //result = factorial(num);
-  //if (result == -1) {
-    //cout << "kertomaa ei voi laskea negatiiviselle luvulle" << endl;
-  //} else {
-    //cout << "Luvun " << num << " Kertoma = " << factorial(num) << endl;
-  //}
+  cout << "Lasketaan kertoma!" << endl;
+  cout << "Anna positiivinen kokonaisluku" << endl;
+  cin >> num;
+  result = factorial(num);
+  if (result == -1) {
+    cout << "kertomaa ei voi laskea negatiiviselle luvulle" << endl;
+  } else {
+    cout << "Luvun " << num << " Kertoma = " << factorial(num) << endl;
+  }
 
-  cout <<"Anna n "<<endl;
-  cin>>n;
-  cout<<"Anna K"<<endl;
-  cin>>k;
-  result=coefficient(n,k);
-  cout<<n<<" yli "<<k<<" = "<<result<<endl;
+  //cout <<"Anna n "<<endl;
+  //cin>>n;
+  //cout<<"Anna K"<<endl;
+  //cin>>k;
+  //result=coefficient(n,k);
+  //cout<<n<<" yli "<<k<<" = "<<result<<endl;
 
   return 0;
 }
